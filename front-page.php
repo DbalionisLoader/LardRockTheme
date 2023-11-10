c58cbb080d145c176b6a771b500bd40b923b3f81
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-11-10 18:16:06 +0000

    Add blank html header to front-page.php

---
+++ front-page.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Black Template">
+    <title>Blank Theme</title>
+    <meta name="author" content="https://dovydasbalionis.co.uk">
+    <!-- Tab icon should go here -->
+    <!-- <link rel="shortcut icon" type="image/svg+xml" href="assets/images/portfavcon-coder.svg"> -->
+
+    <!-- Bootstrap CSS - update to latest if needed -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- Bootstrap Icon - Add here if neededd -->
+    <!-- Page Theme CSS -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>HELLO WORLD</h1>
+</body>
+</html>
--- readme.txt
+++ readme.txt
@@ -1,9 +1,24 @@
-File structure:
+Welcome to LardRock theme - A custom Wordpress theme with a slick name.
+
+FILE structure:
 404.php - Error
 archive.php - Listing page
-comments.php - blog comments
-header.php - navbar?
+search.php - search result page
+comments.php - blog comments section
+header.php - navbar
 footer.php - page footer
-index.php - fallback page
+index.php - main fallback page
 page.php - static page template
 single.php - a single blog post page
+style.css - global styling
+screenshot.png - theme image
+function.php - custom php function and overrides
+front-page.php - homepage/landing page
+
+Reference template hierarchy: https://developer.wordpress.org/files/2014/10/Screenshot-2019-01-23-00.20.04.png
+
+Front-end Frameworks:
+Bootstrap 
+
+
+By D.Balionis
