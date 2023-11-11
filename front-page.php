cd150322b9506b481d90c570f2be29bcaad22694
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-11-11 19:55:13 +0000

    Add a template part for main landing page section

--- front-page.php
+++ front-page.php
@@ -4,15 +4,16 @@
 ?>
 <body>
     <article class="container px-3 py-2">
-        <div><h2><?php the_title(); ?></h2>
-        </div>
+       <!--  <div><h2><?php the_title(); ?></h2>  
+        </div>-->
         <!-- The wordpress loop to fetch page content from the page editor -->
         <!-- Look up other "the" commands to fetch author, post date, tags etc-->
+        <!-- the_content() function outputs the default wordpress blog content-->
         <?php
             if (have_posts()){
                 while (have_posts()){
                     the_post();
-                    the_content();
+                    get_template_part('template-parts/landing','section');
                 }
             }
         ?>
