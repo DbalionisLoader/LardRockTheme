db018ed3970939faf22bf7262d3ce23825182d68
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-11-13 20:12:15 +0000

    Add call to action box and styling
    Add italic text to testimonial section
    Change footer link text size

--- template-parts/landing-section.php
+++ template-parts/landing-section.php
@@ -38,19 +38,20 @@
 </section>
 
 
-<section>
-<div class="container pt-5">
+<section class=>
+<div class="container pb-5 pt-5">
   <div class="text-center mb-5"><h2 class="text-dark">Customer Testimonials</h2></div>
 <!-- Carousel wrapper start here -->
-<div id="carouselExample" class="carousel slide text-center" data-bs-ride="carousel" data-bs-theme="dark">
+<div id="carouselExample" class="carousel slide text-center carousel-size" data-bs-ride="carousel" data-bs-theme="dark">
   <div class="carousel-inner">
+    <!-- ITEM 1 -->
     <div class="carousel-item px-4 active">
       <div class="row d-flex justify-content-center">
         <div class="col-lg-8">
      
           <p class="text-dark">
-            <i class="bi bi-quote pe-2 "></i>
-            There is nowhere better than Tetlows Autos. Never fail to get the job done, diagnostics and then fix is so quick, easy and always affordable. Work is always carried out to a high standard. Thanks Gareth for fixing my baby! She’s driving like a dream 💨   
+            <i class="bi bi-quote pe-2 "></i><em>
+            There is nowhere better than Tetlows Autos. Never fail to get the job done, diagnostics and then fix is so quick, easy and always affordable. Work is always carried out to a high standard. Thanks Gareth for fixing my baby! She’s driving like a dream 💨 </em>  
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote icon-flipped ms-2" viewBox="0 0 16 16">
                 <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
             </svg>
@@ -67,13 +68,14 @@
         <li><i class="bi bi-star-fill"></i></li>
       </ul>
     </div>
+     <!-- ITEM 2 -->
     <div class="carousel-item px-4 ">
       <div class="row d-flex justify-content-center">
         <div class="col-lg-8">
        
           <p class="text-dark">
-            <i class="bi bi-quote pe-2"></i>
-            So grateful to Gareth for sorting my car when it had an oil leak issue - his communication was great, the service he provided was professional & my car seems so much better. Many thanks!
+            <i class="bi bi-quote pe-2"></i><em>
+            So grateful to Gareth for sorting my car when it had an oil leak issue - his communication was great, the service he provided was professional & my car seems so much better. Many thanks! </em> 
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote icon-flipped ms-2" viewBox="0 0 16 16">
                 <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
             </svg>
@@ -89,6 +91,7 @@
         <li><i class="bi bi-star-fill"></i></li>
       </ul>
     </div>
+     <!-- ITEM 3 -->
     <div class="carousel-item px-4 ">
       
       <div class="row d-flex justify-content-center">
@@ -96,9 +99,9 @@
          
       
           <p class="text-dark">
-            <i class="bi bi-quote pe-2"></i>
+            <i class="bi bi-quote pe-2"></i> <em>
             Excellent service, and communication provided by Gareth from start to finish.
-            Reasonable pricing and no messing about. You have gained another future customer 😀
+            Reasonable pricing and no messing about. You have gained another future customer 😀 </em> 
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote icon-flipped ms-2" viewBox="0 0 16 16">
                 <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 9 7.558V11a1 1 0 0 0 1 1h2Zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612c0-.351.021-.703.062-1.054.062-.372.166-.703.31-.992.145-.29.331-.517.559-.683.227-.186.516-.279.868-.279V3c-.579 0-1.085.124-1.52.372a3.322 3.322 0 0 0-1.085.992 4.92 4.92 0 0 0-.62 1.458A7.712 7.712 0 0 0 3 7.558V11a1 1 0 0 0 1 1h2Z"/>
             </svg>
@@ -134,23 +137,20 @@
     <div>
 <!-- Var dump test to see if ACF fields are populated
 //var_dump($values); -->
-
-
-<?php if( have_rows('auto_service_card') ): ?>
- 
- <ul>
- <?php while( have_rows('auto_service_card') ): the_row(); ?>
-    <div><?php 
-    $image = the_sub_field('service_icon_image');
-    $size = 'custom-thumbnail'; 
-    if( $image ) {
-        echo wp_get_attachment_image( $image, $size );}?>
-    </div>
- <li>sub_field_1 = <?php the_sub_field('service_name_title'); ?>, sub_field_2 = <?php the_sub_field('service_description'); ?>, etc</li>
- <?php endwhile; ?>
-</ul>
-<?php endif; ?>
-
-
 </div>
+<section class="mt-5 pt-5 mb-5 mx-2 mx-lg-0">
+  <div class="container shadow rounded call-box p-5 ">
+    <div class="row">
+      <div class="col-12 col-lg-7">
+        <h3 class="mb-4">Reach out to Tetlow Auto's Expert Mobile Mechanics</h3>
+        <h4>For prompt auto support ring on: </h4>
+        <div class="highlight-number">0750 210 1046</div>
+      </div>
+      <div class="col"></div>
+    </div>
+  </div>
+</section>
+
+
+
 
